5d46d60d4e210c0d1e2a281b14bfd7d6962a8776
ytsuji27 <ytsuji91@gmail.com>
2019-08-06 20:25:33 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,50 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+  let updatedQuote;
+  let newVotes;
+
+  switch (action.type) {
+    
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+    
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      if (!quote.votes) {
+        newVotes = 1;
+      } else {
+        newVotes = quote.votes + 1;
+      }
+      updatedQuote = Object.assign({}, quote, { votes: newVotes });
+      return [
+        ...state.slice(0, index),
+        updatedQuote,
+        ...state.slice(index + 1)
+      ];
+
+    case "DOWNVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      if (!quote.votes) {
+        newVotes = 0;
+      } else {
+        newVotes = quote.votes - 1;
+      }
+      updatedQuote = Object.assign({}, quote, { votes: newVotes });
+      return [
+        ...state.slice(0, index),
+        updatedQuote,
+        ...state.slice(index + 1)
+      ];
+
+    default:
+      return state;
+  }
 }
